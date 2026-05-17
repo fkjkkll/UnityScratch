cb826c9455ef9bbfe4d085e14dac70e04d85c89e
Lizq <626409368@qq.com>
2026-05-17 17:29:09 +0800

    测试下Arch

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,13 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using Arch.Core;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
+
+public struct Position
+{
+    public float X;
+    public float Y;
+
+    public override string ToString() => $"{X},{Y}";
+}
 
 public class Test : MonoBehaviour
 {
     void Start()
     {
-        
+        using var world = World.Create();
+        var adventurer = world.Create(new Position() { X = 1, Y = 1 });
+        var query = new QueryDescription().WithAll<Position>();
+        world.Query(in query, (Entity entity, ref Position position) =>
+        {
+            Debug.LogError(position);
+        });
     }
 }
